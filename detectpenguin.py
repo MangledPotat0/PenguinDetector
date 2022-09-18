cccb26f75ad33078ebd01a4c7858123b6ba3d0b1
maelstrom <cephalopodmaster@protonmail.com>
2022-09-18 09:52:11 -0400

    Use different image manipulator

--- detectpenguin.py
+++ detectpenguin.py
@@ -1,12 +1,14 @@
 import keras
 import numpy as np
+import cv2 as cv
 from PIL import Image
 
 def find_penguins(image):
     pd = keras.models.load_model('penguindetector.mdl')
-    data = Image.open(image)
-    size = data.size
-    data = np.resize(data, (1, size[0], size[1], 3))
+    #data = Image.open(image)
+    #size = data.size
+    #data = np.resize(data, (1, size[0], size[1], 3))
+    data = cv.imread(image).resize(1,500,500,3)
     output = pd.predict(data)
     print(output)
 
